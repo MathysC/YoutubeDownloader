8643ffb020342096e25aab3af196928febaaef3a
Mathys Clerget <72934560+MathysC@users.noreply.github.com>
2021-08-09 18:19:16 +0200

    Manager commented

--- Logic/Manager.py
+++ Logic/Manager.py
@@ -1,28 +1,53 @@
 
 from IHM.IHMError import IHMError
 from PyQt5 import QtWidgets, QtCore
+from PyQt5.QtWidgets import QMessageBox
 from pytube import exceptions
 from pytube import YouTube
-import re # Regular Expression
 import os 
 
 
 class Manager():
 
 	def __init__(self):
-		self.folder = "" 
+		"""
+		Constructor of Manager
+		initialize 'folder' and 'url' as str 
+		"""
+		self.folder = os.getcwd() 
 		self.url = ""
 
 
 	def download(func):
+		"""
+		Decorator pattern used for downloading the YouTube url
+		:param func: the function to download the YouTube url
+		:type func: function
+		:return wrapper: the wrapper function
+		:rtype wrapper: function
+		"""
 		def wrapper(self,url):
+			"""
+			The wrapper of this part and the other function depends on the type of download
+			:param url: the url of the YouTube video
+			:type url: str
+			:return val: the 'other' function used in wrapper
+			:rtype val: function
+			:raise exceptions.VideoUnavailable: the video is unavailable
+			:raise exception: if another type of exception raises, create a random advert
+			.. seealso:: self.download_MP3
+			.. seealso:: self.download_MP4
+			.. seealso:: IHMError
+			"""
 			self.url = url
 			if self.verify():
 				try:
 					yt = YouTube(self.url) # By creating a Youtube, if the video is unavailable on YouTube raise a videoUnavailable error
-					val = func(self=self,yt=yt)
+					val = func(self=self,yt=yt) # Call the correct download function
 				except exceptions.VideoUnavailable:
 					IHMError('videoUnavailable_Error')
+				except:
+					IHMError('random')
 					return None
 				finally:
 					return val
@@ -30,15 +55,45 @@ class Manager():
 
 	@download
 	def download_MP4(self,yt):
+		"""
+		This function use the Decorator 'download' to... download mp4
+		:param yt: the YouTube object to download
+		:type yt: pytube.YouTube
+		:return file: the absolute path of the created file
+		:rtype file: str
+		:raise exception: if another type of exception raises, create a random advert
+
+		.. seealso:: IHMError
+		"""
+		file = ""
 		try:
+			# Download the video at its highest resolution
 			stream = yt.streams.filter(progressive=True, file_extension='mp4').order_by('resolution').desc().first()
 			file = stream.download(output_path=self.folder)
-			return file
 		except:
 			IHMError('random')
-
+		finally:
+			# A message appears when the download is complete
+			msg = QMessageBox()
+			msg.setWindowTitle("Téléchargement...")
+			msg.setText("Téléchargement terminé !")
+			msg.setInformativeText(f"Veuillez trouver votre vidéo dans le dossier : {self.folder}")
+			msg.setIcon(QMessageBox.information)
+			msgPop = msg.exec_()
+			return file
 	@download
 	def download_MP3(self,yt):
+		"""
+		This function use the Decorator 'download' to... download mp3
+		:param yt: the YouTube object to download
+		:type yt: pytube.YouTube
+		:return file: the absolute path of the created file
+		:rtype file: str
+		:raise exception: if another type of exception raises, create a random advert
+		
+		.. seealso:: IHMError
+		"""
+		file = ""
 		try:
 			yt = YouTube(self.url) # By creating a Youtube, if the video is unavailable on YouTube raise a videoUnavailable error
 			stream = yt.streams.filter(only_audio=True).first()
@@ -47,25 +102,33 @@ class Manager():
 			os.rename(file,pre + '.mp3')
 		except:
 			IHMError('random')
+		finally:
+			# A message appears when the download is complete
+			msg = QMessageBox()
+			msg.setWindowTitle("Téléchargement...")
+			msg.setText("Téléchargement terminé !")
+			msg.setInformativeText(f"Veuillez trouver votre fichier audio dans le dossier : {self.folder}")
+			msg.setIcon(QMessageBox.Information)
+			msgPop = msg.exec_()
+			return file
 
 	def select_folder(self,ui):
 		"""
-		:param self.folder: the Folder used for the OutCome
-		:type self.folder: str
-		:return path_match: true if the folder exists
-		:rtype: bool
+		:param ui: ui of the MainWindow
+		:type ui: PyQt5 object
+		Change the folder by the chosen one in the directory dialog
 		"""
-		self.folder = QtWidgets.QFileDialog.getExistingDirectory(ui.MainWindow, 'Select Folder')
+		folder = QtWidgets.QFileDialog.getExistingDirectory(ui.MainWindow, 'Select Folder')
+		self.folder = folder  if folder != '' else self.folder # If the user press "Cancel" use the precedent value
 		if self.verify_folder():
 			ui.l_currentFolder.setText(self.folder)
 
 	def verify_folder(self):
 		"""
-		:param self.folder: the Folder used for the OutCome
-		:type self.folder: str
 		:return path_match: true if the folder exists
 			A messageBox appears if false
 		:rtype: bool
+		:raise folder_Error: if the folder path isn't correct
 		"""
 		path_match = os.path.isdir(self.folder)
 		if not path_match:
@@ -75,13 +138,12 @@ class Manager():
 
 	def verify_link(self):
 		"""
-		:param self.url: the 'possible' YouTube URL
-		:type self.url: str
-		:return youtube_regex_match: true if the url is a YouTube URL.
-			A messageBox appears if false
+		:return: True if the url is a YouTube URL.
 		:rtype: bool
+		:raise URL_Error: if the url isn't correct (regex return false)
 			..seealso:: https://stackoverflow.com/questions/4705996/python-regex-convert-youtube-url-to-youtube-video/19161373
 		"""
+		import re
 		youtube_regex = (
 			r'(https?://)?(www\.)?'
 			'(youtube|youtu|youtube-nocookie)\.(com|be)/'
@@ -94,18 +156,32 @@ class Manager():
 			return False
 		return True
 
-	def verify(self):
-		fileMP3 = self.folder + "\\" + YouTube(self.url).title + ".mp3"
-		fileMP4 = self.folder + "\\" + YouTube(self.url).title + ".mp4"
-		b = False
+	def verify_file(self):
+		"""
+		:return: True if the file can be create
+		:rtype: bool
+		:raise FileExistsError: if the file already exists 
+		"""
+
+		# Currently we check if the file exists no matter the extension
+		import glob
 		try:
-			if os.path.isfile(fileMP3) or os.path.isfile(fileMP4):
+			if glob.glob(self.folder + "\\" + YouTube(self.url).title + ".*"):
 				raise FileExistsError
-			else:
-				b = True	
 		except FileExistsError:
 			IHMError("fileExistsError_Error")
-		finally:
-			return b  == self.verify_link() == self.verify_folder()
+			return False
+		return True
+
+	def verify(self):
+		"""
+		:return: True if 
+			the file can be create,
+			the url is a YouTube URL,
+			the folder exists
+		:rtype: bool
+		"""
+
+		return self.verify_file()  == self.verify_link() == self.verify_folder()
 
            
